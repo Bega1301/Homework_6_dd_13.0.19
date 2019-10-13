e67763c363e5c8992d7224552838400e59c2b328
Bega1301 <begaiym13011985@gmail.com>
2019-10-13 18:24:58 +0600

    Initial commit

---
+++ src/com/company/Boss.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Boss extends Player{
+    private String defenceType;
+
+    public String getDefenceType() {
+        return defenceType;
+    }
+
+    public void setDefenceType(String defenceType) {
+        this.defenceType = defenceType;
+    }
+}
---
+++ src/com/company/Magical.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Magical extends Player implements SuperAbility {
+
+    @Override
+    public void superAbility() {
+
+    }
+}
